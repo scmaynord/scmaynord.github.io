2786fdc591f4360569458ae652b1de4d4220235f
scmaynord <105305190+scmaynord@users.noreply.github.com>
2025-09-04 16:58:02 -0500

    Update 20250904.r

--- 20250904.r
+++ 20250904.r
@@ -1,2 +1,10 @@
-#Import Packages
+# Import Packages
 import ggplot2
+
+sales <- read.csv("coffee_sales.csv")
+head(sales)
+
+
+# Prompt written by Silas Maynord
+# "give me a data set and a question to work on my R skills. Specifically data analysis and visualization"
+# https://chatgpt.com/s/t_68ba0a15c76881918e7287d2675b6cb7
